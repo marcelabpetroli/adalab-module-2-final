04962a44dfc13b36a711435c9ae876d195422982
Marcela B Petroli <marcelabpetroli@gmail.com>
2022-11-14 06:19:12 +0100

    Add queries and variables

---
+++ src/js/0_queries.js
@@ -0,0 +1,5 @@
+'use strict';
+const contentResultsElement = document.querySelector('.js_content_results');
+const contentFavElement = document.querySelector('.js_content_fav');
+const searchInputElement = document.querySelector('.js_input');
+const btnSearch = document.querySelector('.js_search_btn');
---
+++ src/js/1_variables.js
@@ -0,0 +1,4 @@
+'use strict';
+// Variables
+let favorites = [];
+let characters = [];
